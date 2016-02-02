cec304333e9d845666a4aead9cbedfaa7c505d98
Andreas Gohr <andi@splitbrain.org>
2016-02-02 16:11:18 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -34,7 +34,7 @@ require_once(DOKU_PLUGIN.'action.php');
  */
 class action_plugin_metaheaders extends DokuWiki_Action_Plugin {
 
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'metaheaders');
     }
 
